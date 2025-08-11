5004b058716720d0007b399a51b409bb077cb9d4
A7MADTANTAWY <ahmedtantawy94z@gmail.com>
2025-08-11 19:37:50 +0300

    setup service locator

---
+++ lib/core/utils/services_locator.dart
@@ -0,0 +1,13 @@
+import 'package:bokly_app/core/utils/api_service.dart';
+import 'package:bokly_app/features/home/data/repos/home_repo_implementation.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerLazySingleton(() => ApiService(Dio()));
+  getIt.registerLazySingleton(
+    () => HomeRepoImplementation(getIt.get<ApiService>()),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:bokly_app/constants.dart';
+import 'package:bokly_app/core/utils/services_locator.dart';
+import 'package:bokly_app/features/home/data/repos/home_repo_implementation.dart';
+import 'package:bokly_app/features/home/presentation/bloc/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bokly_app/features/home/presentation/bloc/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bokly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setup();
   runApp(const BooklyApp());
 }
 
@@ -14,16 +20,28 @@ class BooklyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratAlternatesTextTheme(
-          ThemeData.dark().textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              FeaturedBooksCubit(getIt.get<HomeRepoImplementation>()),
         ),
+        BlocProvider(
+          create: (context) =>
+              NewestBooksCubit(getIt.get<HomeRepoImplementation>()),
+        ),
+      ],
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratAlternatesTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+        ),
+        home: const SplashView(),
       ),
-      home: const SplashView(),
     );
   }
 }
